a3fd0b13e36729790ea909c6efad243ae9275aac
jordyncampbaell <104102347+jordyncampbaell@users.noreply.github.com>
2022-05-10 00:43:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,2 @@
-import './App.css';
 import {useEffect, useState} from "react";
 import * as solanaWeb3 from "@solana/web3.js";
-import imgDiscord from './icons8-discord-50.png'
-import imgTwitter from './icons8-twitter-50.png'
-
